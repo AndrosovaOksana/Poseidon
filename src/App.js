1e8938d8a68d2fb797464a0e10bef498110086e9
Androsova Oksana <oxelika@gmail.com>
2019-05-14 21:03:57 +0300

    change template structure and styles

--- src/App.js
+++ src/App.js
@@ -13,19 +13,68 @@ function App() {
           <div className="row">
             <div className="col-md-12 p3">
               <div className="second_column area">
-                <p>TEST</p>
+                <p>TEST1</p>
               </div>
             </div>
             <div className="col-md-12 p3">
               <div className="third_column area">
-                <ComponentGoogleCharts></ComponentGoogleCharts>
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="block-indicators">
+                      <div className="indicator">
+                        <h4>CPU</h4>
+                        <ComponentGoogleCharts name="CPU"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Network</h4>
+                        <ComponentGoogleCharts name="Network"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Speed</h4>
+                        <ComponentGoogleCharts name="Speed"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Power</h4>
+                        <ComponentGoogleCharts name="Power"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Memory</h4>
+                        <ComponentGoogleCharts name="Memory"></ComponentGoogleCharts>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
+                    <div className="block-indicators">
+                      <div className="indicator">
+                        <h4>CPU</h4>
+                        <ComponentGoogleCharts name="CPU"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Tachometer</h4>
+                        <ComponentGoogleCharts name="Tachometer"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Speed</h4>
+                        <ComponentGoogleCharts name="Speed"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Counter</h4>
+                        <ComponentGoogleCharts name="Counter"></ComponentGoogleCharts>
+                      </div>
+                      <div className="indicator">
+                        <h4>Pressure</h4>
+                        <ComponentGoogleCharts name="Pressure"></ComponentGoogleCharts>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div className="col-md-6 p3">
           <div className="fourth_column area">
-            <p>TEST</p>
+            <p>TEST4</p>
           </div>
         </div>
       </div>
