43be0801a119ed21345d8c62b00bbbb884c3422e
Androsova Oksana <oxelika@gmail.com>
2019-05-14 20:59:13 +0300

    modified component structure

--- src/ComponentGoogleCharts.js
+++ src/ComponentGoogleCharts.js
@@ -1,42 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import {Chart} from 'react-google-charts';
 
-function ComponentGoogleCharts() {
-    const [memory, setMemory] = useState(0);
-    const [cpu, setCpu] = useState(0);
-    const [network, setNetwork] = useState(0);
+function ComponentGoogleCharts(props) {
+    const [state, setState] = useState(0);
     const [intervalID, setIntervalID] = useState(null);
 
     useEffect(() => {
         
                 const intervalID = setInterval(() => {
-                setMemory(Math.random() * 100);
-                setCpu(Math.random() * 100);
-                setNetwork(Math.random() * 100);
+                setState(Math.random() * 100);
                 setIntervalID(intervalID);
             }, 1000);
         
-
         return function() {
             if(intervalID !== null) {
                 clearInterval(intervalID);
             }
         }
-           
+     
     });
     
-
     return (
         <Chart
-        width={400}
-        height={120}
+        width={150}
+        height={150}
         chartType="Gauge"
         loader={<div>Loading Chart</div>}
         data={[
           ['Label', 'Value'],
-          ['Memory', memory],
-          ['CPU', cpu],
-          ['Network', network],
+          [props.name, state],
         ]}
         options={{
           redFrom: 90,
